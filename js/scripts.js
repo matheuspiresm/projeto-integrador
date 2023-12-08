cc62bd4a57fd7609ce914d8f5eea87adb02a5706
Matheus Leonardo P. Modesto <mattpires@icloud.com>
2023-12-08 13:28:17 -0300

    feat: adiciona conteudo da tela inicial

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,32 @@ function toggleMenu(){
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+
+/*JS do carrossel*/ 
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const items = document.querySelectorAll('.carousel-item');
+  const totalItems = items.length;
+
+  function showItem(index) {
+      items.forEach(item => item.style.display = 'none');
+      items[index].style.display = 'block';
+  }
+
+  function nextItem() {
+      currentIndex = (currentIndex + 1) % totalItems;
+      showItem(currentIndex);
+  }
+
+  function prevItem() {
+      currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+      showItem(currentIndex);
+  }
+
+  // Iniciar o carrossel
+  showItem(currentIndex);
+  
+  // intervalo para avançar automaticamente
+  setInterval(nextItem, 3500);  
+});
