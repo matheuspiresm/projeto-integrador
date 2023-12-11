91c41ebf36e6cf1e2082ead694ffb749816c5514
Sr.K <82061876+kaianfreitas@users.noreply.github.com>
2023-12-11 00:38:55 -0300

    atualizações 11/12

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,39 @@
+console.log("wassup");
+
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu() {
   const nav = document.querySelector('nav');
   nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+
+/*JS do carrossel*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const items = document.querySelectorAll('.carousel-item');
+  const totalItems = items.length;
+
+  function showItem(index) {
+    items.forEach(item => item.style.display = 'none');
+    items[index].style.display = 'block';
+  }
+
+  function nextItem() {
+    currentIndex = (currentIndex + 1) % totalItems;
+    showItem(currentIndex);
+  }
+
+  function prevItem() {
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+    showItem(currentIndex);
+  }
+
+  // Iniciar o carrossel
+  showItem(currentIndex);
+
+  // intervalo para avançar automaticamente
+  setInterval(nextItem, 3100);
+});
